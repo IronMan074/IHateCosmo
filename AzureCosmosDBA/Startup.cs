80a573df3f9924ac7f77120ad5fbdab2793a87fc
Nikhiel Chetty <21642914@dut4life.ac.za>
2018-04-30 01:49:25 +0200

    Add project files.

---
+++ AzureCosmosDBA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureCosmosDBA.Startup))]
+namespace AzureCosmosDBA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
